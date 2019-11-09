9dc89fdff1711cfecdbe97daec673cfff9882cd9
erichua <erichua@webank.com>
2019-11-09 20:16:44 +0800

    support theme

--- src/views/SceneView.js
+++ src/views/SceneView.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-export default class SceneView extends React.PureComponent {
+export default class SceneView extends React.Component {
   static childContextTypes = {
     navigation: propTypes.object.isRequired,
   };
+  
+  static contextTypes = {
+    theme: propTypes.object.isRequired 
+  }
 
   getChildContext() {
     return {
       navigation: this.props.navigation,
     };
+  }
+
+  shouldComponentUpdate (nextProps: Readonly<P>, nextState: Readonly<S>, nextContext: any): boolean {
+    return nextContext && this.context && nextContext.theme !== this.context.theme
   }
 
   render() {
